af81a49e589a98df42c753b84fcb6205b1441a65
develqhy <ikettle9000@163.com>
2024-03-05 12:50:54 +0800

    项目搭建-弃用MyBatis Plus,整合tkMapper

--- xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/ServiceContentApplication.java
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/ServiceContentApplication.java
@@ -3,11 +3,13 @@ package com.xuecheng;
 import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
 
-// 启动项目后访问地址: http://localhost:9100/content/swagger-ui.html
+// 启动项目后访问地址: http://localhost:5100/content/swagger-ui.html
 
 @SpringBootApplication
 @EnableSwagger2Doc
+@MapperScan("com.xuecheng.content.mapper")
 public class ServiceContentApplication {
 
     public static void main(String[] args) {
